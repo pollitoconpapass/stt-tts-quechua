345e71e894160a00b8a5d02423d6d4eb9c63632a
Jose A. Quispe <jalbertoqz@gmail.com>
2024-03-01 15:08:22 -0500

    first-commit

--- stt_facebook.py
+++ stt_facebook.py
@@ -1,7 +1,6 @@
 from transformers import Wav2Vec2ForCTC, AutoProcessor
 import torchaudio
 import torch
-import io
 
 
 def stt(wave_file_path):  
@@ -9,7 +8,7 @@ def stt(wave_file_path):
     processor = AutoProcessor.from_pretrained(model_id)
     model = Wav2Vec2ForCTC.from_pretrained(model_id)
 
-    audio_data, original_sampling_rate = torchaudio.load(io.BytesIO(wav_file_path.read()))
+    audio_data, original_sampling_rate = torchaudio.load((wave_file_path))
     resampled_audio_data = torchaudio.transforms.Resample(original_sampling_rate, 16000)(audio_data)
     inputs = processor(resampled_audio_data.numpy(), sampling_rate=16000, return_tensors="pt")
 
